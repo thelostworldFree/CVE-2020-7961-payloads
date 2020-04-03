cd50001718c91efc36bcd8906d61499b0da04042
Thisisfarhadzadeh <en.farhadzadeh@gmail.com>
2020-04-03 15:00:15 -0400

    Initial comment

---
+++ LifExp.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class LifExp {
+
+static {
+
+//You should put your codes in this section Like 'Liferay_Windows_payload.txt' 
+
+}
+}
